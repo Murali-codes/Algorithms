3b3259fcdaf1dc44827dbcdc176a35d0a40f9ac8
Murali Krishna Gundoji <muralikrishna.gundoji@factset.com>
2020-04-21 22:11:39 +0530

    alternate for isSubsequence

--- src/CodingExcercises/isSubsequence.ts
+++ src/CodingExcercises/isSubsequence.ts
@@ -19,11 +19,22 @@ export const isSubsequence = (str1, str2) => {
       j++;
     }
   }
-  console.log(i, j);
+
   if (i === str1.length) {
     return true;
   }
   return false;
 };
+
+/*
+
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}
+
+*/
 
 console.log(isSubsequence("hello", "hello world"));
